64777c084926ebd71926be5e6fed2d9585456d08
RupuLI <46904000+RupuLI@users.noreply.github.com>
2020-02-13 21:04:50 -0500

    Add files via upload

--- EE551_HW1.py
+++ EE551_HW1.py
@@ -6,23 +6,29 @@ Python Core object Types
 
 import math
 
+
 def numbers_and_strings():
-    """
-    This is to review numbers and strings and basic operations.
-    """
+    # """
+    # This is to review numbers and strings and basic operations.
+    # """
     # Assign a string "EE551" to a variable x
     x = "EE551"
-
+    print(x)
     # Assign a string "Stevens" to a variable y
-
+    y = "Stevens"
+    print(y)
     # Repeat variable y 5 times
-
+    z = y * 5
+    print(z)
     # What is the length of z?
-
+    length = len(z)
+    print(length)
     # Concatenate variable y with string " is good"
-
+    m = y + " is good"
+    print(m)
     # Replace "good" with "awesome" in variable m and assign it to a new variable n
-
+    n = m.replace("good", "awesome")
+    print(n)
     return x, y, z, length, m, n
 
 
@@ -33,22 +39,29 @@ def lists():
     n = "Stevens is awesome"
 
     # Split variable n on a delimiter space into a list of substrings
-
+    p = list(n.split(" "))
+    print(p)
     # Get all the items past the first of the third substring
-
+    r = p[2][1:]
+    print(r)
     # Create a 3 x 3 matrix as nested list such that
     #   first row is [1, 4, 5]
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
-
+    a = [[1, 4, 5], [6, 10, 11], [12, 17, 38]]
+    print(a)
     # Collect the items in the last column of matrix A using list comprehension
-
+    c = [k[-1]for k in a]
+    print(c)
     # Collect only the even items of the diagonal of matrix A using list comprehension
-
+    d = [a[i][i]for i in range(0, 3) if a[i][i] % 2 == 0]
+    print(d)
     # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
     # by passing it to the built-in ord function. Generate a list of these integers to represent
     # each character of the string "Stevens" using list comprehension.
-
+    s = "Stevens"
+    o = [ord(s[i])for i in range(0, len(s))]
+    print(o)
     return p, r, c, d, o
 
 
@@ -60,31 +73,31 @@ def dictionaries():
     #   fruit => "apple"
     #   quantity => 4
     #   color => "green"
-
+    a = {'fruit': 'apple', 'quantity': 4, 'color': 'green'}
+    print(a)
     # Get the item in dictionary f that the key "fruit" maps to
-
+    f = a['fruit']
+    print(f)
     # Increase the quantity of f by 1
     # IMPLEMENT IT HERE
-
+    p = a['quantity'] = a['quantity'] + 1
+    print(p)
     # Create a nested dictionary where:
     #   name => {first_name => "Grace", last_name => "Hopper"} (a dictionary)
     #   jobs => ["scientist", "engineer"] (a list)
     #   age => 85
-
+    amazing_grace = {'name': {'first_name': "Grace", 'last_name': "Hopper"}, 'jobs': ["scientist", "engineer"], 'age': 85}
     # Add "programmer" to the list of jobs Grace has
     # IMPLEMENT IT HERE
-
+    amazing_grace['jobs'].append('programmer')
     # Get the third job Grace has that you recently added
-
+    k = amazing_grace['jobs'][-1]
+    print(k)
     # Use the sort() function to get sorted keys of amazing_grace in alphabetically ascending order
-
+    print(sorted(amazing_grace.keys()))
     return a, f, p, k
+
 
 numbers_and_strings()
 lists()
 dictionaries()
-
-
-
-
-
